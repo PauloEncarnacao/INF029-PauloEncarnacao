e45ba6f2e8f2bbad6de91ed7a2ce5885da65433d
PauloEncarnacao <112086535+PauloEncarnacao@users.noreply.github.com>
2022-08-30 18:14:10 +0000

    q11

--- revisao geral/qq11.c
+++ revisao geral/qq11.c
@@ -1,2 +1,34 @@
 /*11. 
 Criar e armazenar ua matriz a 10x10 e gerar e imprimir a matriz invertida*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 2	
+int main (){
+
+int matriz[TAM][TAM];
+int i,j;
+
+for(i=0; i<=TAM; i++)
+{
+	for( j=0; j<=TAM; j++)
+	{
+		printf("\n[%d %d]",i,j);
+		scanf("%d",&matriz[i][j]);
+	}
+}
+
+for(i=TAM; i>=0; i--)
+{
+	for( j=TAM; j>=0; j--)
+	{
+		printf("\n[%d %d]\n",i,j);
+		printf(" %d ",matriz[i][j]);
+		
+	}
+}
+return 0;
+}
+	
+	
