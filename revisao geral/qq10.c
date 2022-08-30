b0d444c0af9b58aab421776d174f0998ec653b4d
PauloEncarnacao <112086535+PauloEncarnacao@users.noreply.github.com>
2022-08-30 20:18:11 +0000

    outro

--- revisao geral/qq10.c
+++ revisao geral/qq10.c
@@ -1,41 +1,4 @@
-/*10. 
+10. 
 Criar programa que leia e armazena os elementos de uma matriz M inteira
 10x10 e imprima todos os elementos que estão em linhas pares e colunas
-impares.*/
-
-
-
-#include <stdio.h>
-#include <string.h>
-
-#define TAM 10	
-int main (){
-
-int matriz[TAM][TAM];
-int i,j;
-
-for(i=0; i<TAM; i++)
-{
-	for( j=0; j<TAM; j++)
-	{
-		printf("[%d %d]",i,j);
-		scanf("%d",&matriz[i][j]);
-	}
-}
-
-  for(i=0; i<TAM; i++)
-{
-	for( j=0; j<TAM; j++)
-	{
-	if(j%2==0 && i%2!=0) 
-	{
-	printf("[%d",matriz[i][j]);
-	}
-	
-	}	
-}
-
-return 0;
-}
-	
-	
+impares.
