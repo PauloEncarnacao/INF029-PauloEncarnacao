9b36ce7860903a697451b0608b8a0903fd458877
PauloEncarnacao <112086535+PauloEncarnacao@users.noreply.github.com>
2022-08-30 01:34:34 +0000

    news

--- revisao geral/q6.c
+++ revisao geral/q6.c
@@ -1,3 +1,57 @@
-6. Armazenar nomes e nota 1, 2, de 15 alunos. calcular e armazenar a media.
+/*6. Armazenar nomes e nota 1, 2, de 15 alunos. calcular e armazenar a media.
 armazenar tambem a situação do aluno: AP ou RP. imprimir uma listagem
-contendo nome, notas, media e a situação de cada aluno.
+contendo nome, notas, media e a situação de cada aluno.*/
+
+  
+  
+  
+#include <stdio.h>
+#include <string.h>
+
+
+struct aluno_situacao{
+  char nome[50];
+  float nota1;
+  float nota2;
+  float media;
+}alunos[15];
+
+int main()
+{
+
+int media;
+    for(int i=0; i<16; i++)
+      {
+printf("digite o nome do aluno  ");
+fgets(alunos[i].nome,50,stdin);
+
+printf("digite a nota 1  ");
+scanf("%f",&alunos[i].nota1);
+
+printf("digite a nota 2  ");
+scanf("%f",&alunos[i].nota2);
+getchar();
+
+alunos[i].media=(alunos[i].nota1 + alunos[i].nota2)/2;
+// media
+
+}
+      
+   for (int j=0; j<=16; j++)
+   {
+     
+     printf("o aluno %s\n",alunos[j].nome);
+     printf("obteve a media = %.2f\n",alunos[j].media);
+     if(alunos[j].media>=7)
+     {
+     
+     printf("aprovado");     
+ }
+ 
+ else
+ printf("reprovado");
+   }   
+      
+  return 0;
+  
+}
