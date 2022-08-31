e4cde86885edfb58ff23f42664a73bf28f41c807
PauloEncarnacao <112086535+PauloEncarnacao@users.noreply.github.com>
2022-08-31 21:30:19 +0000

    mudancas

--- lista funcoes/q4.c
+++ lista funcoes/q4.c
@@ -2,42 +2,5 @@
 usuário e retornar os três números. A função main deve imprimir esses três números. Utilize vetor
 para fazer o retorno.*/
 
-
 #include <stdio.h>
-
-struct tres_numeros{
-    int vetor[2];
-}numero;
-
-int ler_3_numeros(int num )
-{
-   //capturando os dados
-   for(int i=0 ;i<3; i++)
-   {
-    printf("Digite o valor dos numeros");
-    scanf("%d",&numero.vetor[i]);
-   }
-     //imprimindo os dados 
-   for(int i=0; i<2; i++)
-   {
-    printf("Digite o valor dos numeros %d\n",numero.vetor[i]);
-   }
-   
-   
-    return numero;
-}
-
-int main()
-{
-    
-    int reposta=ler_3_numeros();
-     for(int i=0; i<3; i++)
-     {
-         printf("%d\n",resposta);
-     }
-    
-    
-    return 0;
-}
-
 
--- revisao geral/q9.c
+++ revisao geral/q9.c
@@ -1,3 +1,33 @@
 
-  9. 
-Criar programa que leia elementos de uma matriz inteira de 10x10 e escreva os elementos da diagonal principal
+ /* 9. 
+Criar programa que leia elementos de uma matriz inteira de 10x10 e escreva os elementos da diagonal principal*/
+
+
+#
+
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 3	
+int main (){
+
+int matriz[TAM][TAM];
+int i,j;
+
+for(i=0; i<TAM; i++)
+{
+	for( j=0; j<TAM; j++)
+	{
+		printf("[%d %d]",i,j);
+		scanf("%d",&matriz[i][j]);
+	}
+}
+
+for(i=0; i<TAM; i++)
+{
+
+	printf("%d\t",matriz[i][i]);
+}
+  return 0;
+}
+  
