1baed1f8770b1eb516113110c33f8e12d81d5b7f
PauloEncarnacao <112086535+PauloEncarnacao@users.noreply.github.com>
2022-09-14 19:40:51 +0000

    escolaa

--- escola/funcoes.h
+++ escola/funcoes.h
@@ -1,9 +1,172 @@
-Aluno cadastro_aluno();
-Professor cadastro_professor();
-Disciplina cadastro_disciplina();
-void printa_aluno();
-void printa_professor();
-void printa_disciplina();
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stddef.h>
+
+#define CADASTRADO_SUCESSO 1
+#define ATUALIZADO_SUCESSO 4
+
+#define ERRO_CADASTRO_MATRICULA 2
+#define ERRO_CADASTRO_SEXO 3
+#define ERRO_DATA_DE_NASCIMENTO 0
+
+#define TAM 3
+
+struct dma {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+// struct dados dos alunos
+typedef struct dados_aluno {
+    int matricula;
+    char nome[100];
+    char sexo;
+    struct dma data_nascimento;
+    char cpf[12];
+} Aluno;
+
+// struct dados dos professores
+typedef struct dados_professor {
+    int matricula;
+    char nome[100];
+    char sexo;
+    struct dma data_nascimento;
+    char cpf[12];
+} Professor;
+
+// struct dos dados das disciplinas, separados por letra e numero
+typedef struct codigo {
+    char letra[5];
+    char numero[5];
+} Codigo;
+
+// struct dos dados da disciplina
+typedef struct dados_disciplinas {
+    char nome[53];
+    struct codigo cod;
+} Disciplina;
+
+Aluno aluno; 
+Professor professor;
+Disciplina disciplina;
 
 
-//cabeçalho das funcoes
+void MenuPrincipal(){
+
+    printf("\n");
+    printf("---------------------------------\n");
+    printf("------ MENU PRINCIPAL -----------\n");
+    printf(" 0 - Sair\n");
+    printf(" 1 - GERENCIAR ALUNO \n");
+    printf(" 2 - GERENCIAR PROFESSOR \n");
+    printf(" 3 - GERENCIAR DISCIPLINA \n");
+    printf(" 4 - GERAR RELATORIO \n");
+    printf("---------------------------------\n");
+    printf("DIGITE UMA DAS OPCOES: ");
+
+}
+
+
+void MenuSecundario(){
+
+    printf("\n");
+    printf("---------------------------------\n");
+    printf(" 0 - SAIR\n");
+    printf(" 1 - INCLUIR\n");
+    printf(" 2 - ATUALIZAR\n");
+    printf(" 3 - EXCLUIR \n");
+    printf("---------------------------------\n");
+    printf("Digite opcao desejada: ");
+
+}
+// função de incluir aluno!!!
+int incluir_aluno (Aluno lista_aluno[], int qtd_alunos) {
+
+
+        printf("Insira a matricula: ");
+        scanf("%d", &lista_aluno[qtd_alunos].matricula);
+        getchar();
+
+        if(lista_aluno[qtd_alunos].matricula <= 0){
+            return ERRO_CADASTRO_MATRICULA;
+        }
+
+        printf("Insira o nome: ");
+        fgets(lista_aluno[qtd_alunos].nome, 100, stdin);
+        size_t ln = strlen(lista_aluno[qtd_alunos].nome) -1;
+        if(lista_aluno[qtd_alunos].nome[ln] == '\n')
+            lista_aluno[qtd_alunos].nome[ln] = '\0';
+
+        printf("Insira o Sexo: ");
+        printf("Utilize as letras 'M' para masculino, 'F' para feminino e 'O' para outros");
+        scanf("%c", &lista_aluno[qtd_alunos].sexo);
+
+        lista_aluno[qtd_alunos].sexo = toupper(lista_aluno[qtd_alunos].sexo);
+        if(lista_aluno[qtd_alunos].sexo != 'M' & lista_aluno[qtd_alunos].sexo != 'F'){
+            return ERRO_CADASTRO_SEXO;
+        }
+
+        printf("Data de nascimento\n ");
+        printf("Dia: ");
+        scanf("%d", &lista_aluno[qtd_alunos].data_nascimento.dia);
+
+        printf("Mes: ");
+        scanf("%d", &lista_aluno[qtd_alunos].data_nascimento.mes);
+
+        printf("Ano: ");
+        scanf("%d", &lista_aluno[qtd_alunos].data_nascimento.ano);
+        getchar();
+
+	//validação da data
+        //if(ValidaData(lista_aluno[qtd_alunos].data_nascimento.dia, lista_aluno[qtd_alunos].data_nascimento.mes, lista_aluno[qtd_alunos].data_nascimento.ano) != 0){
+            
+			printf("Digite o CPF: ");
+            fgets(lista_aluno[qtd_alunos].cpf, 12, stdin); 
+            ln = strlen(lista_aluno[qtd_alunos].cpf) - 1; 
+            if (lista_aluno[qtd_alunos].cpf[ln] == '\n')
+            lista_aluno[qtd_alunos].cpf[ln] = '\0';
+
+         else {
+            return ERRO_DATA_DE_NASCIMENTO;
+        }
+
+        printf("\n");
+
+    return CADASTRADO_SUCESSO;
+}
+
+
+
+
+int atualizar_aluno(Aluno lista_aluno[],int qtd_alunos)	{
+	
+	
+	return ATUALIZADO_SUCESSO;
+}
+
+int ValidaData(int dia, int mes, int ano){
+/* data inválida quando a variável 'validade' retorna 0*/
+    int validade;
+
+	if (mes > 12 || mes < 1 || dia < 1 || dia > 31){
+		validade = 0;
+	}
+	else if ((dia < 1 && dia > 31) && (mes == 1||mes == 3||mes == 5||mes == 7||mes == 8||mes == 10||mes == 12)) {
+		validade = 0;
+	}
+	else if ((dia < 1 && dia > 30) && (mes == 4|| mes == 6 || mes == 9 || mes == 11)){
+        	validade = 0;
+    	}
+	else if (ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0)) {
+		if (mes == 2 && dia > 29) {
+			validade = 0;
+		}
+	}
+	else if (mes == 2 && dia > 28) {
+		validade = 0;
+	}
+	return validade;
+}
--- escola/main.c
+++ escola/main.c
@@ -1,47 +1,343 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include <string.h>
+#include <ctype.h>
+#include <stddef.h>
 #include "funcoes.h"
 
-	int opcao;
-	int sair=0;
-	//incializo o menu com o while
+#define TAMANHO 3
 
-int main()
-{
-	while(!sair){
-		printf("\nDigite a opcao: \n ");
-		printf("0 - Sair\n");
-    	printf("1 - Inserir Aluno\n");
-   		printf("2 - Listar Alunos\n");
-   		printf("3 - Inserir Professor\n");
-   		printf("4 - Listar Professores\n");
-   		printf("5 - Inserir Disciplna\n");
-   		printf("6 - Listar Disciplinas\n");
-    	scanf("%d",&opcao);
-    
-    switch(opcao)
-    {
-    	case 1:
-    		cadastro_aluno();
-			break;
-		case 2:
-			printa_aluno();
-			break;
-			
-		case 3:
-			cadastro_professor();
-			break;
-		case 4:
-			printa_professor();
-			break;
-		case 5:
-			cadastro_disciplina();
-			break;
-		case 6:
-			printa_disciplina();
-			break;
-	}
-}
-}
+void MenuPrincipal();
+void MenuSecundario();
+void MenuRelatorio();
+int incluirAluno (Aluno lista_aluno[], int qtd_alunos);
+int atualizarAluno(Aluno lista_aluno[], int qtd_aluno, int matricula);
+int ValidaData(int dia, int mes, int ano);
+
+int main(){
 
 
+    Aluno lista_aluno[TAMANHO];
+    int qtd_alunos = 0;
+
+
+    Professor lista_prof[TAMANHO];
+    int qtd_prof = 0;
+
+
+    Disciplina lista_disci[TAMANHO];
+    int qtd_disciplina = 0;
+
+    int sair_menu = 0;
+    int opcao;
+
+    while (!sair_menu){
+        MenuPrincipal();
+        scanf("%d", &opcao);
+
+        switch (opcao){
+            case 0 :{
+                printf("Finalizando...\n");
+                sair_menu = 1;
+                break;
+            }
+
+            case 1: {
+                int sair_aluno = 0;
+                int opcao_aluno;
+
+                printf("----> ALUNO <---- \n");
+
+                while (!sair_aluno){
+                    MenuSecundario();
+                    scanf("%d", &opcao_aluno);
+
+                    switch (opcao_aluno){
+                        case 0: {
+                            printf("Finalizando Aluno...\n");
+                            sair_aluno = 1;
+                            break;
+                        }
+
+                        case 1: {
+                            int resultado;
+                            printf(" Incluir...\n");
+
+                            resultado = incluirAluno(lista_aluno, qtd_alunos);
+
+                                if(resultado == CADASTRADO_SUCESSO){
+                                    printf("Cadastrado com sucesso!\n");
+                                    qtd_alunos++;
+                                } else {
+                                    switch(resultado) {
+                                        case ERRO_CADASTRO_MATRICULA:{
+                                            printf("Matrícula Inválida");
+                                            break;
+                                        }
+                                        case ERRO_CADASTRO_SEXO:{
+                                            printf("Sexo Inválido");
+                                            break;
+                                        }
+
+                                        case ERRO_DATA_DE_NASCIMENTO: {
+                                            printf("Data de nascimento inválida");
+                                            break;
+                                        }
+
+                                        default:{
+                                            printf("Erro desconhecido!");
+                                        }
+                                    }
+
+                                printf("Não foi possível fazer o cadastro\n"); 
+
+                                }
+
+                            printf("\n");
+
+                            break;
+                        }
+
+                        case 2: {
+                            printf("Atualizar...\n");
+                            break;
+                        }
+
+                        case 3: {
+                            printf("Deletar...\n");
+                            break;
+                        }
+
+                        default:{
+                            break;
+                        }
+                    }
+                }
+
+                break;
+            }
+          }
+
+/*           
+			case 2: {
+                int sair_professor = 0;
+                int opcao_prof;
+
+                printf("----> PROFESSOR <----\n");
+
+                while (!sair_professor){
+                    MenuSecundario();
+                    scanf("%d", &opcao_prof);
+
+                    switch (opcao_prof){
+                        case 0: {
+                            printf("Finalizando Professor...\n");
+                            sair_professor = 1;
+                            break;
+                        }
+
+                        case 1: {
+                            int resultado;
+
+                            printf(" Incluir...\n");
+
+                           // resultado = incluirProfessor(lista_prof, qtd_prof);
+
+                            if(resultado == CADASTRADO_SUCESSO){
+                                    printf("Cadastrado com sucesso!\n");
+                                    qtd_prof++;
+                                } else {
+                                    switch(resultado) {
+                                        case ERRO_CADASTRO_MATRICULA:{
+                                            printf("Matrícula Inválida");
+                                            break;
+                                        }
+
+                                        case ERRO_CADASTRO_SEXO:{
+                                            printf("Sexo Inválido");
+                                            break;
+                                        }
+
+                                        case ERRO_DATA_DE_NASCIMENTO: {
+                                            printf("Data de nascimento inválida");
+                                            break;
+                                        }
+
+                                        default:{
+                                            printf("Erro desconhecido!");
+                                        }
+                                    }
+
+                                printf("Não foi possível fazer o cadastro\n"); 
+
+                                }
+
+                            printf("\n");
+
+                            break;
+                        }
+
+                        case 2: {
+                            printf("Atualizar...\n");
+                            break;
+                        }
+
+                        case 3: {
+                            printf("Deletar...\n");
+                            break;
+                        }
+
+                        default:{
+                            printf("Opcao incorreta, tente novamente...\n");
+                            break;
+                        }
+                    }
+                }
+
+                break;
+            }
+
+            case 3: {
+
+                int sair_disciplina = 0;
+                int opcao_disci;
+
+                printf("----> DISCIPLINA <----\n");
+
+                while (!sair_disciplina){
+                    MenuSecundario();
+                    scanf("%d", &opcao_disci);
+
+                    switch (opcao_disci){
+                        case 0: {
+                            printf("Finalizando Disciplina...\n");
+                            sair_disciplina = 1;
+                            break;
+                        }
+
+                        case 1: {
+                            int resultado; 
+                            printf(" Incluir...\n");
+                            //incluirDisciplina(lista_disci, qtd_disciplina);
+
+                            if (resultado == CADASTRADO_SUCESSO){
+                                printf("Disciplina cadastrada com sucesso!\n");
+                                //qtd_disciplina++;
+                            }
+                            break;
+                        }
+
+                        case 2: {
+                            printf("Atualizar...\n");
+                            break;
+                        }
+
+                        case 3: {
+                            printf("Deletar...\n");
+                            break;
+                        }
+
+                        default:{
+                            printf("Opcao incorreta, tente novamente...\n");
+                            break;
+                        }
+                    }
+                }
+
+                break;
+            }
+
+            case 4: {
+                int sair_relatorio = 0;
+                int opcao_relatorio;
+
+                printf("----> RELATÓRIOS <----\n");
+
+                while (!sair_relatorio){
+                    MenuRelatorio();
+                    scanf("%d", &opcao_relatorio);
+
+                    switch (opcao_relatorio){
+                        case 0: {
+                            printf("Saindo Relatorios...\n");
+                            sair_relatorio = 1; 
+                            break;
+                        }
+
+                        case 1: {
+                            printf("Listando Alunos...\n");
+                           // listarAlunos(lista_aluno, qtd_alunos);
+                            break;
+                        }
+
+                        case 2: {
+                            printf("Listando Professores...\n");
+                           // listarProfessor(lista_prof, qtd_prof);
+                            break;
+                        }
+
+                        case 3: {
+                            printf("Listando Disciplinas...\n");
+                            //listarDisciplina(lista_disci, qtd_disciplina);
+                            break;
+                        }
+
+                        case 4: {
+                            printf("Listar Alunos por sexo\n");
+                            printf("Página em construção\n");
+                            break;
+                        }
+
+                        case 5: {
+                            printf("Listar Alunos ordenados por nome\n");
+                            printf("Página em construção\n");
+                            break;
+                        }
+
+                        case 6: {
+                            printf("Listar Alunos ordenados por data de nascimento\n");
+                            printf("Página em construção\n");
+                            break;
+                        }
+
+                        case 7: {
+                            printf("Listar Professores por sexo\n");
+                            printf("Página em construção\n");
+                            break;
+                        }
+
+                        case 8: {
+                            printf("Listar Professores ordenados por nome\n");
+                            printf("Página em construção\n");
+                            break;
+                        }
+
+                        case 9: {
+                            printf("Listar Professores ordenados por data de nascimento\n");
+                            printf("Página em construção\n");
+                            break;
+                        }
+
+                        default:{
+                            printf("Opcao incorreta, tente novamente...\n");
+                            break;
+                        }
+
+                    }
+                }
+
+                break;
+            }
+
+            default: {
+                printf("Opcao incorreta, tente novamente!\n");
+                break;
+            }
+        }
+    }
+*/
+    }
+        }
+    return 0;
+  }
+      
